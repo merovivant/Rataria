ae8a0337c29605be737a7b6f8735b6510010dade
Jonatas de Sousa Santos <j225223@dac.unicamp.br>
2023-06-24 18:07:18 -0300

    Adicionando textura aos tijolos e contador de vida aos ratos

--- config.py
+++ config.py
@@ -25,3 +25,31 @@ class Config:
     font = pygame.font.SysFont("arialblacomicck", 40)
     COR_FONTE = (255, 255, 255)
     COR_FUNDO_TEXTO = (0, 0, 0)
+
+    # imagens que serão usadas durante o jogo
+    LIFE = lambda x: pygame.transform.scale(pygame.image.load(f'imgs/lives/{x}.png'), (0.5*Config.BLOCK_SIZE*(199/57), 0.5*Config.BLOCK_SIZE))
+
+    menu1_jpg = pygame.image.load("imgs/menu1.jpg")
+    menu2_jpg = pygame.image.load("imgs/menu2.jpg")
+    leaderboardFUNDO_jpg = pygame.image.load("imgs/leaderboard2.jpg")
+    
+    campanha_jpg = pygame.image.load("imgs/botoes/campanha.png")
+    arcade_jpg = pygame.image.load("imgs/botoes/arcade.png")
+    leaderboard_jpg = pygame.image.load("imgs/botoes/leaderboard.png")
+    sair_jpg = pygame.image.load("imgs/botoes/sair.png")
+    escKey_jpg = pygame.image.load("imgs/botoes/escKey.png")
+    back_jpg = pygame.image.load("imgs/botoes/back.png")
+
+    campanhaAlt_jpg = pygame.image.load("imgs/botoes/campanhaAlt.png")
+    arcadeAlt_jpg = pygame.image.load("imgs/botoes/arcadeAlt.png")
+    leaderboardAlt_jpg = pygame.image.load("imgs/botoes/leaderboardAlt.png")
+    sairAlt_jpg = pygame.image.load("imgs/botoes/sairAlt.png")
+
+    # Botões que serão usados 
+    botao_campanha = Button(254, 280, campanha_jpg, campanhaAlt_jpg, 0.5)
+    botao_arcade = Button(254, 100, arcade_jpg, arcadeAlt_jpg, 0.5)
+    botao_leaderboard = Button(254, 190, leaderboard_jpg, leaderboardAlt_jpg, 0.5)
+    botao_sair = Button(254, 600, sair_jpg, sairAlt_jpg, 0.5)
+    
+    botao_esc = Button(254, 600, escKey_jpg, escKey_jpg, 0.5)
+    botao_back = Button(45, 45, back_jpg, back_jpg, 0.5)
